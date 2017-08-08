2afad50436dc6ed689c14d22d6985b1587789505
Elene Akhvlediani <eakhv11@freeuni.edu.ge>
2017-08-08 19:46:00 +0400

    speech recognition

--- VoiceCalculator/ViewController.swift
+++ VoiceCalculator/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 import Speech
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    lazy var calculator = Calculator.sharedInstance
 
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer:SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,8 +29,41 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didClickListenButton(_ sender: UIButton) {
+        recordAndRecognizeSpeech()
+    }
+    
+    func recordAndRecognizeSpeech() {
+        guard let node = audioEngine.inputNode else { return}
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, _) in
+            self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+            
+        }catch {
+            print(error)
+            return
+        }
         
+        guard  let myRecognizer  = speechRecognizer else {
+            return
+        }
+        if !myRecognizer.isAvailable {
+            return
+        }
         
+        recognitionTask  = myRecognizer.recognitionTask(with: request, resultHandler: { (result, error) in
+            if let result = result {
+                let bestString = result.bestTranscription.formattedString
+                print(bestString)
+                
+            }else if let error = error {
+                print(error)
+            }
+        })
+
     }
 
 }
